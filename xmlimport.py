444a6936af4208247e3c90e0654cc3862270752a
stianchris <cbrosig@posteo.de>
2019-05-09 15:45:09 +0200

    cleared docstring

--- xmlimport.py
+++ xmlimport.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Copyright 2018-2019 Christian Brosig (TH Köln), Timo Platte (TH Köln)
+# Copyright 2018-2019 Christian Brosig (TH Köln)
 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
